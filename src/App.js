00fc35108985c3a87cc454dcd1a8df26d15d81fc
leole <leo.leduff@gmail.com>
2022-08-14 19:41:05 +0200

    adjacent nodes array update (in progress) + css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import Map from "./Map";
 import { v4 as uuidv4 } from 'uuid';
 
+const WIDTH = 30;
+const HEIGHT = 17;
 
 function initiateMap(length) {
     const newMap = Array(length);
@@ -11,25 +13,33 @@ function initiateMap(length) {
     return newMap;
 }
 
-function App() {
-    const [ map, setMap ] = useState(initiateMap(504))
+function initiateAdjacent(map) {
+    const adjacent = map.map(node => node.active && node.id);
+    console.log(adjacent)
+    return adjacent;
+ }
 
-    const [ lastFirstSelectedState, setLastFirstSelectedState ] = useState();
+function App() {
+    const [ map, setMap ] = useState(initiateMap(WIDTH * HEIGHT));
+    const [ lastFirstSelectedState, setLastFirstSelectedState ] = useState(true);
+    const [ adjacent, setAdjacent ] = useState([]);
+    const
+
 
     //called only on mouseDown
     function changeFirstNode(id) {
+        const newMap = [...map];
         const node = map.find(node => node.id === id);
         setLastFirstSelectedState(node.active);
-        changeNodeState(node.id);
+        node.active = !node.active;
+        setMap(newMap);
     }
 
     function changeNodeState(id) {
         const newMap = [...map];
-        if (node === null) {
-            node = newMap.find(node => node.id === id);
-            if (node.active !== currentNodeState) {     //change only nodes which are in the same state that the first one clicked
-                return;
-            }
+        const node = newMap.find(node => node.id === id);
+        if (node.active !== lastFirstSelectedState) {     //change only nodes which are in the same state that the first one clicked
+            return;
         }
         node.active = !node.active;
         setMap(newMap);
@@ -46,7 +56,9 @@ function App() {
             <div>
                 <button onClick={handleClear}>clear</button>
             </div>
-            <Map map={map} changeNodeState={changeNodeState} changeFirstNode={changeFirstNode}/>
+            <div style={{display:"flex", justifyContent: "center"}}>
+                <Map map={map} changeNodeState={changeNodeState} changeFirstNode={changeFirstNode}/>
+            </div>
         </>
     );
 }
--- src/Map.js
+++ src/Map.js
@@ -5,9 +5,11 @@ import './App.css';
 export default function Map({ map, changeNodeState, changeFirstNode }) {
     return (
         <>
-            {map.map(node => {
-                return <Node key={node.id} node={node} changeNodeState={changeNodeState} changeFirstNode={changeFirstNode}/>
-            })}
+            <div className={`map`}>
+                {map.map(node => {
+                    return <Node key={node.id} node={node} changeNodeState={changeNodeState} changeFirstNode={changeFirstNode}/>
+                })}
+            </div>
         </>
     );
 }
